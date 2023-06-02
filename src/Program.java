fdc2d8a15f4a96c2663599de38c45ff9e5d6a077
AstonDV <astondv.kms@gmail.com>
2023-06-02 22:36:08 +1000

    Write program

--- src/Program.java
+++ src/Program.java
@@ -7,18 +7,26 @@ public class Program {
         products.add(new Snack("Snickers", 0.75, "Mars"));
         products.add(new Drink("Coca-Cola", 1.00, "Regular"));
         products.add(new HotDrink("Coffee", 1.50, 80));
+        products.add(new HotDrink("Tea", 1.00, 70));
+        products.add(new HotDrink("Hot Chocolate", 2.00, 75));
 
-        DrinkVendingMachine vendingMachine = new DrinkVendingMachine();
-        vendingMachine.initProducts(products);
+        DrinkVendingMachine vendingMachine1 = new DrinkVendingMachine();
+        vendingMachine1.initProducts(products);
+
+        HotDrinkVendingMachine vendingMachine2 = new HotDrinkVendingMachine();
+        vendingMachine2.initProducts(products);
 
         // Получение продуктов по имени
-        System.out.println(vendingMachine.getProduct("Coca-Cola"));
-        System.out.println(vendingMachine.getProduct("Snickers"));
+        System.out.println(vendingMachine1.getProduct("Coca-Cola"));
+        System.out.println(vendingMachine1.getProduct("Snickers"));
 
         // Получение воды по имени и объему
-        System.out.println(vendingMachine.getProduct("Evian", 500));
+        System.out.println(vendingMachine1.getProduct("Evian", 500));
 
         // Получение горячего напитка по имени, объему и температуре
-        System.out.println(vendingMachine.getProduct("Coffee", 0, 80));
+        System.out.println(vendingMachine2.getProduct("Coffee", 0, 80));
+        System.out.println(vendingMachine2.getProduct("Tea", 0, 70));
+        System.out.println(vendingMachine2.getProduct("Hot Chocolate", 0, 75));
+
     }
 }
